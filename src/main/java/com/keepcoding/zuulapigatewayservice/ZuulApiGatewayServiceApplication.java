5f5fe6d866bb90cf598121a920a8eeb515155368
Jose Luis Solis <solis.jl@gmail.com>
2020-07-21 09:06:51 -0700

    Initial commit

---
+++ src/main/java/com/keepcoding/zuulapigatewayservice/ZuulApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.keepcoding.zuulapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayServiceApplication.class, args);
+	}
+
+}
